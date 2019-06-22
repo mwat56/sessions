8f29abec100f517f53478b654006e9b824042e99
Matthias Watermann <support@mwat.de>
2019-06-22 18:26:17 +0200

    * included `TSession` definition and methods;

--- sessions.go
+++ sessions.go
@@ -14,82 +14,130 @@ import (
 	"time"
 )
 
+type (
+	// `tSessionData` stores the session data.
+	tSessionData map[string]interface{}
+
+	// TSession is a `map`-based session store
+	TSession struct {
+		sData tSessionData
+		sID   string
+	}
+)
+
+// Delete removes the session data identified by `aKey`.
+func (so *TSession) Delete(aKey string) error {
+	// If m is nil or there is no such element, delete is a no-op.
+	delete(so.sData, aKey)
+
+	return nil
+} // Delete()
+
+// Get returns the session data identified by `aKey`.
+//
+// If `aKey` doesn't exist the method returns `nil`.
+func (so *TSession) Get(aKey string) interface{} {
+	if result, ok := so.sData[aKey]; ok {
+		return result
+	}
+
+	return nil
+} // Get()
+
+// SessionID returns the session's ID.
+func (so *TSession) SessionID() string {
+	return so.sID
+} // SessionID()
+
+// Set adds/updates the session data of `aKey` with `aValue`.
+//
+// This implementation always returns `nil`.
+func (so *TSession) Set(aKey string, aValue interface{}) error {
+	so.sData[aKey] = aValue
+
+	return nil
+} // Set()
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// `newSession()` returns a new `TMapSession` instance.
+func newSession(aSID string) *TSession {
+	result := TSession{
+		sData: make(tSessionData),
+		sID:   aSID,
+	}
+
+	return &result
+} // newSession()
+
 var (
 	// `defaultLifetime` is the lifetime for an unused session.
-	// It defaults to 30 minutes.
+	// It defaults to 1800 seconds (30 minutes).
 	defaultLifetime = int64(60 * 30)
-)
 
-type (
-	// tTSession is the type to get/set session data.
-	tTSession interface {
+	// sessionHandler is the global session handler.
+	sessionHandler *TSessionHandler
 
-		// Get returns the session data identified by `aKey`.
-		Get(aKey string) interface{}
-
-		// Set adds/updates the session data of `aKey` with `aValue`.
-		Set(aKey string, aValue interface{}) error
-
-		// Delete removes the session data identified by `aKey`.
-		Delete(aKey string) error
-
-		// SessionID returns the session's ID.
-		SessionID() string
-	}
-
-	// tTSessionHandler defines the interface of a session handler.
-	tTSessionHandler interface {
-
-		// Init (initialise) the session.
-		//
-		// `aSavePath` The path where to store/retrieve the session data.
-		Init(aSavePath string) error
-
-		// Close the session.
-		Close() error
-
-		// Load reads the session data from disk.
-		//
-		// `aSID` The session ID to read data for.
-		Load(aSID string) (*tTSession, error)
-
-		// Store writes session data to disk..
-		//
-		// `aSID` The current session ID.
-		// `aValue` The session data to store.
-		Store(aSession *tTSession) error
-
-		// Destroy a session.
-		//
-		// `aSID` The session ID being destroyed.
-		Destroy(aSID string) error
-
-		// GC cleans up old sessions.
-		GC() error
-	}
-)
-
-var (
-	// Sessions is the global session handler.
-	Sessions *TSessionHandler
+	// `sidName` is the GET/POST identifier fo the session ID.
+	sidName = "SID"
 )
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-// DefaultLifetime returns the default max. lifetime of a session.
+// DefaultLifetime returns the default max. TTL of a session.
 func DefaultLifetime() int64 {
 	return defaultLifetime
 } // DefaultLifetime()
 
-// `newID()` returns an ID based on time and random bytes.
-func newID() string {
+// Delete removes the session data of `aSID` identified by `aKey`.
+//
+// `aSID` identifies the session to use.
+// `aKey` is the key to access the wanted data.
+func Delete(aSID, aKey string) error {
+	return sessionHandler.Delete(aSID, aKey)
+} // Delete()
+
+// Get returns the session data of `aSID` identified by `aKey`.
+//
+// `aSID` identifies the session to use.
+// `aKey` is the key to access the wanted data.
+func Get(aSID, aKey string) interface{} {
+	return sessionHandler.Get(aSID, aKey)
+} // Get()
+
+// GetSession returns the `TSession` for `aSID`.
+//
+// `aSID` identifies the session to return.
+func GetSession(aSID string) *TSession {
+	result, _ := sessionHandler.Load(aSID)
+
+	return result
+} // GetSession()
+
+// NewSession returns a new `TSession` instance.
+func NewSession() *TSession {
+	result, _ := sessionHandler.Load(newSID())
+
+	return result
+} // NewSession()
+
+// `newSID()` returns an ID based on time and random bytes.
+func newSID() string {
 	b := make([]byte, 16)
 	rand.Read(b)
 	id := fmt.Sprintf("%d%s", time.Now().UnixNano(), b)
 	b = []byte(id[:24])
 
 	return base64.URLEncoding.EncodeToString(b)
-} // newID()
+} // newSID()
+
+// Set adds/updates the session data of `aKey` with `aValue`.
+//
+// `aSID` identifies the session to use.
+// `aKey` is the key to access the wanted data.
+func Set(aSID, aKey string, aValue interface{}) error {
+	return sessionHandler.Set(aSID, aKey, aValue)
+} // Set()
 
 // SetDefaultLifetime sets the default max. lifetime of a session.
 //
@@ -102,6 +150,20 @@ func SetDefaultLifetime(aMaxLifetime int64) {
 	}
 } // SetDefaultLifetime()
 
+// SetSIDname sets the session name.
+//
+// `aSID` identifies the session data.
+func SetSIDname(aSID string) {
+	if 0 < len(aSID) {
+		sidName = aSID
+	}
+} // SetSIDname
+
+// SIDname returns the configured session name.
+func SIDname() string {
+	return sidName
+} // SIDname()
+
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 // Wrap initialises the session handling.
@@ -110,28 +172,24 @@ func SetDefaultLifetime(aMaxLifetime int64) {
 //
 // `aSessionDir` is the name of the directory to store session files.
 func Wrap(aHandler http.Handler, aSessionDir string) http.Handler {
-	Sessions, _ = newFilehandler(aSessionDir)
+	sessionHandler, _ = newFilehandler(aSessionDir)
 
 	return http.HandlerFunc(
 		func(aWriter http.ResponseWriter, aRequest *http.Request) {
 			var usersession *TSession
 
-			// (1) get `aSID` from `aRequest`
-			// store `aSID` as `aRequest.Header["Cookie"]` (or "SID"?)
-			if sid := aRequest.FormValue("SID"); 0 < len(sid) {
-				// load session file
-				usersession, _ = Sessions.Load(sid)
+			sid := aRequest.FormValue("SID")
+			if 0 == len(sid) {
+				sid = newSID()
 			}
+			// load session file from disk
+			usersession, _ = sessionHandler.Load(sid)
 
-			// (2) init session handling for `aSID`
-			// provide session access (how?)
-
-			// (3) call wrapped `ServeHttp()` method
+			// the original handler can access the session now
 			aHandler.ServeHTTP(aWriter, aRequest)
 
-			// (4) close session for `aSID`
-			Sessions.Store(usersession)
-
+			// save the (possibly modified) session data
+			go sessionHandler.Store(usersession)
 		})
 } // Wrap()
 
--- sessions_test.go
+++ sessions_test.go
@@ -18,9 +18,35 @@ func Test_newID(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := newID(); len(got) != tt.want {
+			if got := newSID(); len(got) != tt.want {
 				t.Errorf("newID() = %v, want %v", len(got), tt.want)
 			}
 		})
 	}
 } // Test_newID()
+
+func Test_newSession(t *testing.T) {
+	sid := "aTestSID"
+	w1 := &TSession{
+		sData: tSessionData{},
+		sID:   sid,
+	}
+	type args struct {
+		aSID string
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TSession
+	}{
+		// TODO: Add test cases.
+		{" 1", args{sid}, w1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newSession(tt.args.aSID); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newSession() = %v,\nwant %v", got, tt.want)
+			}
+		})
+	}
+} // Test_newSession()
