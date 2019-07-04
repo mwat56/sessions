7e99c2b185053f19801dec82d9185e231ebf009c
Matthias Watermann <support@mwat.de>
2019-07-04 16:43:58 +0200

    * completed implementation of `tHRefWriter.appendSID()`;

--- href.go
+++ href.go
@@ -7,42 +7,71 @@
 package sessions
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
+	"regexp"
+	"strings"
 )
 
 type (
 	// `tLogWriter` embeds a `ResponseWriter`
 	tHRefWriter struct {
 		http.ResponseWriter // used to construct the HTTP response
-		session             *TSession
+		sID                 string
 	}
+
+	// `tBoolLookup` is a simple binary lookup table
+	tBoolLookup map[bool]string
 )
 
-// Write writes the data to the connection as part of an HTTP reply.
-//
-// Part of the `http.ResponseWriter` interface.
-func (hr *tHRefWriter) Write(aData []byte) (int, error) {
-	aData = hr.appendSID(aData)
+var (
+	// RegEx to match complete link tags
+	hrefRE = regexp.MustCompile(`(?si)(<a[^>]*href=")([^"]+)("[^>]*>)`)
 
-	return hr.ResponseWriter.Write(aData)
-} // Write()
+	// lookup table for appending CGI argument
+	lookupCGIchar = tBoolLookup{true: "&", false: "?"}
+)
 
 // `appendSID()` appends the current session ID to all `a href` tags.
 //
 // `aData` The web/http response.
 func (hr *tHRefWriter) appendSID(aData []byte) []byte {
-	// cgi := fmt.Sprintf("%s=%s", sidName, hr.session.ID())
+	linkMatches := hrefRE.FindAllSubmatch(aData, -1)
+	if nil == linkMatches {
+		return aData
+	}
+	cgi := fmt.Sprintf("%s=%s", sidName, hr.sID)
 	/*
 		There are three cases to consider:
-			(a) links to external pages
+			(a) links to external pages (ignored)
 			(b) links to internal pages w/o CGI arguments
 			(c) links to internal pages with CGI arguments
-
 	*/
+	for l, cnt := len(linkMatches), 0; cnt < l; cnt++ {
+		if 0 == len(linkMatches[cnt][2]) { // the URL to check
+			continue
+		}
+		if strings.HasPrefix(string(linkMatches[cnt][2]), "http") {
+			continue // skip links to external sites
+		}
+		repl := fmt.Sprintf("%s%s%s%s%s",
+			linkMatches[cnt][1],
+			linkMatches[cnt][2],
+			lookupCGIchar[0 < bytes.IndexRune(linkMatches[cnt][2], '?')],
+			cgi,
+			linkMatches[cnt][3])
+		aData = bytes.ReplaceAll(aData, linkMatches[cnt][0], []byte(repl))
+	}
 
 	return aData
 } // appendSID()
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+// Write writes the data to the connection as part of an HTTP reply.
+//
+// Part of the `http.ResponseWriter` interface.
+func (hr *tHRefWriter) Write(aData []byte) (int, error) {
+	return hr.ResponseWriter.Write(hr.appendSID(aData))
+} // Write()
 
 /* _EoF_ */
---
+++ href_test.go
@@ -0,0 +1,51 @@
+/*
+   Copyright © 2019 M.Watermann, 10247 Berlin, Germany
+                  All rights reserved
+               EMail : <support@mwat.de>
+*/
+
+package sessions
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_tHRefWriter_appendSID(t *testing.T) {
+	sid := "aTestSIDvalue"
+	h1 := tHRefWriter{sID: sid}
+	d0 := []byte(`bla bla bla`)
+	w0 := d0
+	d1 := []byte(`Bla bla <a title="Link(1)" href="page1.html">Link(1)</a>`)
+	w1 := []byte(`Bla bla <a title="Link(1)" href="page1.html?` + string(sidName) + `=` + sid + `">Link(1)</a>`)
+	d2 := []byte(`Bla bla <a title="Link(2)" href="http://example.com/page2.html">Link(2)</a>`)
+	w2 := d2
+	d3 := []byte(`Bla bla <a title="Link(3)" href="page3.html?k=v">Link(3)</a>`)
+	w3 := []byte(`Bla bla <a title="Link(3)" href="page3.html?k=v&` + string(sidName) + `=` + sid + `">Link(3)</a>`)
+	d4 := []byte(string(d1) + string(d2) + string(d3))
+	w4 := []byte(string(w1) + string(w2) + string(w3))
+	type args struct {
+		aData []byte
+	}
+	tests := []struct {
+		name   string
+		fields tHRefWriter
+		args   args
+		want   []byte
+	}{
+		// TODO: Add test cases.
+		{" 0", h1, args{d0}, w0},
+		{" 1", h1, args{d1}, w1},
+		{" 2", h1, args{d2}, w2},
+		{" 3", h1, args{d3}, w3},
+		{" 4", h1, args{d4}, w4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hr := &tt.fields
+			if got := hr.appendSID(tt.args.aData); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("tHRefWriter.appendSID() = %s,\nwant %s", got, tt.want)
+			}
+		})
+	}
+} // Test_tHRefWriter_appendSID()
